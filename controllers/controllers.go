7f78230067da5c0b44c0c0e7f4fb126839b6e856
Pedro Paccola <paccolapedro@gmail.com>
2023-01-08 12:10:07 -0300

    aula 5

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -41,9 +41,35 @@ func CreateStudent(c *gin.Context) {
 	c.JSON(http.StatusOK, student)
 }
 
+func UpdateStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+
+	database.DB.First(&student, id)
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	database.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, student)
+}
+
+func DeleteStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.Delete(&student, id)
+	c.JSON(http.StatusOK, gin.H{
+		"data": "student deleted successfully",
+	})
+}
+
 func Greetings(c *gin.Context) {
 	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"api": "greetings " + name + " !",
 	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,8 @@ func HandleRequests() {
 	r.GET("/students", controllers.GetAllStudents)
 	r.POST("/students", controllers.CreateStudent)
 	r.GET("/students/:id", controllers.GetStudentById)
+	r.DELETE("/students/:id", controllers.DeleteStudent)
+	r.PATCH("/students/:id", controllers.UpdateStudent)
 	r.GET("/:name", controllers.Greetings)
 	r.Run()
 }
